d24d59b6d0a46e4317ec89a5e8d7b4cf57a2027a
Aylen1198 <aylengenova1@gmail.com>
2024-08-19 12:17:59 -0300

    clase02

---
+++ src/componentes/ProductoCard/ProductoCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductoImagen from '../ProductoImagen/ProductoImagen'
+
+const ProductoCard = () => {
+  return (
+    <div>
+        <ProductoImagen/>
+    </div>
+  )
+}
+
+export default ProductoCard
